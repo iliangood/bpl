0fad5aa4668066983dea2048da009e17e2de68d4
iliangood <ilia191168@gmail.com>
2025-11-15 19:39:35 +0300

    работа над парсингом

--- src/interpreter/parser.cpp
+++ src/interpreter/parser.cpp
@@ -1,7 +1,8 @@
 #include "interpreter/parser.h"
+#include <sstream>
 
-Parser::Parser(Processor processor)
-	: processor_(&processor), globalScope_(), scopes_()
+Parser::Parser(Processor* processor)
+	: processor_(processor), globalScope_(), scopes_()
 {}
 
 std::optional<Argument> Parser::parseArgument(std::vector<std::string>::const_iterator* it, const std::vector<std::string>::const_iterator& end)
@@ -9,9 +10,50 @@ std::optional<Argument> Parser::parseArgument(std::vector<std::string>::const_it
 	if(*it == end)
 		throw std::runtime_error("Unexpected end of program while parsing argument");
 	Argument arg;
-	if((*it)->starts_with("value"))
+	std::stringstream ss(**it);
+	std::vector<std::string> parts;
+	std::string part;
+	while(std::getline(ss, part, ':'))
 	{
-
+		parts.push_back(part);
+	}
+	if(parts.size() == 0)
+		throw std::runtime_error("Invalid argument format: " + **it);
+	if(parts[0] == "Value")
+	{
+		if(parts[1] == "Function" && parts.size() >= 3)
+		{
+			Function func;
+			std::vector<Instruction> body;
+			FunctionType funcType;
+			
+			++(*it);
+			while(**it != ")")
+			{
+			}
+			
+			return arg;
+		}
+		if(parts.size() != 3)
+			throw std::runtime_error("Invalid Value argument format: " + **it);
+		if(parts[1] == "int64")
+		{
+			int64_t value = std::stoll(parts[2]);
+			arg = Value(value);
+		}
+		else if(parts[1] == "char")
+		{
+			if(parts[2].size() != 1)
+				throw std::runtime_error("Invalid char Value argument format: " + **it);
+			char value = parts[2][0];
+			arg = Value(value);
+		}
+		else
+		{
+			throw std::runtime_error("Unknown Value type: " + parts[1]);
+		}
+		++(*it);
+		return arg;
 	}
 }
 
